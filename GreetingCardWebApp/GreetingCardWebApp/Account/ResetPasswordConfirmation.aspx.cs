81b7f174d3631b82197f99070db3d6e97cb90063
BirenS <soni.biren@gmail.com>
2016-08-11 11:31:00 +0530

    New Project is added to the repository

---
+++ GreetingCardWebApp/GreetingCardWebApp/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace GreetingCardWebApp.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ GreetingCardWebApp/GreetingCardWebApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GreetingCardWebApp.Startup))]
+namespace GreetingCardWebApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
